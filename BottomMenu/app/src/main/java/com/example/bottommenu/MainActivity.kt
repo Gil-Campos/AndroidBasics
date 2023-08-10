aef5d31966c9a904bbf1064fc1284807b370886b
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-08-10 16:45:23 -0600

    Add an example on how to use a BottomNav along side with a NavigationComponent

---
+++ BottomMenu/app/src/main/java/com/example/bottommenu/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.bottommenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.bottommenu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavMain,
+            findNavController(R.id.fragment_container)
+        )
+
+    }
+}
