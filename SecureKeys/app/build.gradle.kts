8b682adc3860fb98308e528a26d6b2ff551352e3
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-09-24 14:29:24 -0600

    Finish an example on how to save securely API Keys in the app

--- SecureKeys/app/build.gradle.kts
+++ SecureKeys/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.securekeys"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.securekeys"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -24,6 +24,17 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            buildConfigField("String", "GOOGLE_MAPS_API_KEY", "testKey123")
+        }
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+            buildConfigField("String", "GOOGLE_MAPS_API_KEY", "\"qwerty12345\"")
         }
     }
     compileOptions {
@@ -33,15 +44,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx: 1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
 }
--- SecureKeys/app/src/main/java/com/example/securekeys/MainActivity.kt
+++ SecureKeys/app/src/main/java/com/example/securekeys/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.securekeys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private var apiKey = BuildConfig.GOOGLE_MAPS_API_KEY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This prints the key to check
+        Log.d("MAIN_API_KEY", apiKey)
     }
 }
